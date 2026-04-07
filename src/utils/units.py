4f212067c93f19dd565abf695f7896b4ceb07619
ameoli <alessandromeoli@gmail.com>
2026-04-07 21:40:16 +0200

    drop unused helpers from utils/units.py

--- src/utils/units.py
+++ src/utils/units.py
@@ -1,20 +1,11 @@
+"""Small unit conversion helpers."""
+
 from utils.constants import C
-import numpy as np
-
-
-def wavelength_to_frequency(wl):
-    """Wavelenght [m] to frequency [Hz]"""
-    return C / wl
 
 
 def dB_to_linear(dB):
     """Power dB to linear"""
     return 10 ** (dB / 10)
-
-
-def dB_to_amplitude(dB):
-    """Power dB to amplitude (field)"""
-    return 10 ** (dB / 20)
 
 
 def wavelength_range_to_freq_range(center_wl, range_wl):
